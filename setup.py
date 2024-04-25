3d3b5aabbd86c7bae5f07c581032688c68318aa9
Sergey Rezvanov <im@rezwyi.ru>
2024-04-25 13:37:26 +0500

    Updates

--- setup.py
+++ setup.py
@@ -1,42 +1,5 @@
 #!/usr/bin/env python
-# coding: utf-8
-from sentry_telegram_py3 import __version__
+
 from setuptools import setup
 
-with open('README.rst', 'r') as f:
-    long_description = f.read()
-
-
-setup(
-    name='sentry_telegram_py3',
-    version=__version__,
-    packages=['sentry_telegram_py3'],
-    url='https://github.com/vortland/sentry-telegram',
-    author='Vladislav Bukhman',
-    author_email='rpechka@gmail.com',
-    description='Plugin for Sentry which allows sending notification via Telegram messenger.',
-    long_description=long_description,
-    long_description_content_type='text/x-rst',
-    license='MIT',
-    entry_points={
-        'sentry.plugins': [
-            'sentry_telegram_py3 = sentry_telegram_py3.plugin:TelegramNotificationsPlugin',
-        ],
-    },
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Topic :: Software Development :: Bug Tracking',
-        'Topic :: Software Development :: Quality Assurance',
-        'Topic :: System :: Monitoring',
-    ],
-    include_package_data=True,
-)
+setup()
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,15 +1,14 @@
-# coding: utf-8
 import pytest
 
 from exam import fixture
 import json
 import responses
 from sentry.models import Rule
-from sentry.plugins.base import plugins, Notification
+from sentry.plugins.base import Notification
 from sentry.testutils import PluginTestCase
 
 
-from sentry_telegram_py3.plugin import TelegramNotificationsPlugin
+from sentry_telegram.plugin import TelegramNotificationsPlugin
 
 
 class BaseTest(PluginTestCase):
@@ -17,82 +16,84 @@ class BaseTest(PluginTestCase):
     def plugin(self):
         return TelegramNotificationsPlugin()
 
-
     def test_conf_key(self):
         assert self.plugin.conf_key == "sentry_telegram_py3"
 
     @responses.activate
     def test_complex_send_notification(self):
-        responses.add(responses.POST, "https://api.telegram.org/botapi:token/sendMessage")
-        self.plugin.set_option('api_origin', 'https://api.telegram.org', self.project)
-        self.plugin.set_option('receivers', '123', self.project)
-        self.plugin.set_option('api_token', 'api:token', self.project)
+        responses.add(
+            responses.POST, "https://api.telegram.org/botapi:token/sendMessage"
+        )
+        self.plugin.set_option("api_origin", "https://api.telegram.org", self.project)
+        self.plugin.set_option("receivers", "123", self.project)
+        self.plugin.set_option("api_token", "api:token", self.project)
         self.plugin.set_option(
-            'message_template',
-            '*[Sentry]* {project_name} {tag[level]}: {title}\n{message}\n{url}',
+            "message_template",
+            "*[Sentry]* {project_name} {tag[level]}: {title}\n{message}\n{url}",
             self.project,
         )
 
         event = self.store_event(
             data={
-                "message": "Hello world", 
+                "message": "Hello world",
                 "level": "error",
                 "platform": "python",
-            }, 
-            project_id=self.project.id
+            },
+            project_id=self.project.id,
         )
         rule = Rule.objects.create(project=self.project, label="my rule")
-        
+
         notification = Notification(event=event, rule=rule)
 
         with self.options({"system.url-prefix": "http://example.com"}):
             self.plugin.notify(notification)
-        
+
         request = responses.calls[0].request
         print(request)
         print(request.url)
         print(request.body)
 
-        message_text = '*[Sentry]* Bar error: Hello world\n' \
-                            'Hello world\n' \
-                            'http://example.com/organizations/baz/issues/1/'
+        message_text = (
+            "*[Sentry]* Bar error: Hello world\n"
+            "Hello world\n"
+            "http://example.com/organizations/baz/issues/1/"
+        )
 
-        assert request.url == 'https://api.telegram.org/botapi:token/sendMessage'
+        assert request.url == "https://api.telegram.org/botapi:token/sendMessage"
 
         payload = json.loads(request.body)
         assert payload == {
-            'text': message_text,
-            'parse_mode': 'Markdown',
-            'chat_id': '123',
+            "text": message_text,
+            "parse_mode": "Markdown",
+            "chat_id": "123",
         }
-        
 
     @staticmethod
     def assert_notification_helper(request_call, message_text):
         assert request_call == dict(
             allow_redirects=False,
-            method='POST',
-            headers={'Content-Type': 'application/json'},
-            url='https://api.telegram.org/botapi:token/sendMessage',
+            method="POST",
+            headers={"Content-Type": "application/json"},
+            url="https://api.telegram.org/botapi:token/sendMessage",
             json={
-                'text': message_text,
-                'parse_mode': 'Markdown',
-                'chat_id': '123',
+                "text": message_text,
+                "parse_mode": "Markdown",
+                "chat_id": "123",
             },
             timeout=30,
             verify=True,
         )
 
     def test_get_empty_receivers_list(self):
-        self.plugin.set_option('receivers', '', self.project)
+        self.plugin.set_option("receivers", "", self.project)
         assert self.plugin.get_receivers(self.project) == []
 
     def test_get_config(self):
         self.plugin.get_config(self.project)
 
     def test_is_configured(self):
-        self.plugin.set_option('receivers', '123', self.project)
-        self.plugin.set_option('api_token', 'api:token', self.project)
+        self.plugin.set_option("receivers", "123", self.project)
+        self.plugin.set_option("api_token", "api:token", self.project)
         assert self.plugin.is_configured(self.project)
 
     def test_is_not_configured(self):
