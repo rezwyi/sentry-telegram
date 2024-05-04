a52b08335d85ee5018acddc96835e8e796e1904e
Sergey Rezvanov <im@rezwyi.ru>
2024-05-04 16:59:11 +0500

    Fix "drain_shard should only be called outside of any active transaction!" error

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,13 @@
+import pytest
+
 pytest_plugins = [
     "sentry.testutils.pytest",
 ]
+
+
+@pytest.fixture(autouse=True)
+def setup_simulate_on_commit(request):
+    from sentry.testutils.hybrid_cloud import simulate_on_commit
+
+    with simulate_on_commit(request):
+        yield
